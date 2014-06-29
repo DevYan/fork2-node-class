ab85949b550339e945abf058330b2035d9a36158
DevYan <3027873@163.com>
2014-06-29 17:29:45 +0800

    Implement Class Inheritance

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
-var Class = function(){
+var pro_Name = 'initialize';
+
+var extend = function(child,parent) {
+  if(typeof parent === 'undefined'){
+    parent = Object;
+  }
+
+  child.__super__ = parent;
+  child.prototype.__proto__ == parent.prototype;
+
+  var currentClass = child;
+  child.prototype.super = function(methodName){
+    var args = [].splice.call(arguments,1);
+    currentClass = currentClass.__super__;
+    var result = currentClass.prototype[methodName].apply(this,args);
+    currentClass = child;
+    return result;
+  }
+}
+
+var Class = function( args , parent){
+  function ctor() {
+    if (args.hasOwnProperty(pro_Name)){
+      args[pro_Name].apply(this,arguments);
+    }
+  }
+
+  for (var key in args){
+    if(!args.hasOwnProperty(key)){
+      continue;
+    }
+    if(key === pro_Name){
+      continue;
+    }
+    ctor.prototype[key] = args[key];
+  }
+
+  extend(ctor,parent);
+
+  return ctor;
+}
+
+
+/*var Class = function(){
   var result = function result(){};
   var len = arguments.length;
   
@@ -14,7 +57,7 @@ var Class = function(){
   }
 
   return result;
-}
+}*/
 
 
 module.exports = Class;
